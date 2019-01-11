b83389fd5e58e5a7645e198744c928dcd0c1b111
tibbs001 <sheri.tibbs@duke.edu>
2019-01-10 23:11:44 -0500

    sort and downcase the collection of names to ignore

--- app/models/lookup/sponsor.rb
+++ app/models/lookup/sponsor.rb
@@ -73,136 +73,136 @@ module Lookup
     end
 
     def self.names_to_ignore
-      [
-        'arctec',
-        'anand prasad',
-        'christian kern',
-        'carol johnston',
-        'salvador gil-vernet',
+      [        'argos'                         ,  # 'q4789707 british retail company
+        'baronova, inc.'                ,  # 'q2467415 ballerina
+        'brendan lee'                   ,  # 'q4960963 australian rules footballer
+        'brian walters'                 ,  # 'q4965569 australian politician
+        'brigade de sapeurs pompiers de paris'       ,  # 'q583422 brigade
+        'byron lam'                     ,  # 'q5004408 australian botanist
+        'caroline rowland'              ,  # 'q5045208 british film producer
+        'cengiz kaya'                   ,  # 'q6037195 basketball player
+        'christopher sweeney, mbbs'     ,  # 'q17306850 british director
+        'colaris, joost, m.d.'          ,  # 'q1107605 band
+        'david a. johnson, md'          ,  # 'q1038597 baseball player
+        'david birnie'                  ,  # 'q377241 australian serial killer
+        'henry c. lin, md'              ,  # 'q21453388 british painter (1877-1965)
+        'jacob rosenberg'               ,  # 'q6119196 australian writer
+        'jenny tong, md, mph'           ,  # 'q334348 british politician
+        'johannes wacker, md'           ,  # 'q27906334 badminton player
+        'joseph bliss'                  ,  # 'q6281570 british politician
+        'julie page'                    ,  # 'q1639835 british basketball player
+        'kenneth hargreaves'            ,  # 'q6390187 british army officer
+        'kevin davy'                    ,  # 'q6396118 british musician
+        'krystal parker'                ,  # 'q6439758 british footballer
+        'lucy holmes, md'               ,  # 'q6698364 australian singer
+        'mary knudson, m.d.'            ,  # 'q58051223 anthropologist
+        'michael gallagher'             ,  # 'q6830494 british miner, politician and
+        'michael levy'                  ,  # 'q337010 british baron
+        'nicholas kenyon'               ,  # 'q7025709 british journalist
+        'oliver strohm'                 ,  # 'q686257 austrian ski jumper
+        'sa ambulance service'          ,  # 'q7388884 australian ambulance service
+        'shashi bhatt, md'              ,  # 'q13487172 badminton player
+        'abdulkadir tunc'               ,  # 'q15043411 turkish actor
+        'adam friedman'                 ,  # 'q2823944 american television producer a
+        'airfan'                        ,  # 'q55760547 association football player
         'alisa apreleva',
-        'victoria gomez',
-        'yijing he',
-        'rachel little',
-        'balamurali ambati',
-        'vincent kan',
-        'joel lavine',
+        'alison walker'                 ,  # 'q4727205 british sports broadcaster
+        'anand prasad',
+        'andreas guenther'              ,  # 'q497666 german actor
+        'andrew brenner'                ,  # 'q4756413 american politician
+        'arctec',
         'arctic',
-        'nancy bauman',
-        'lfce',
-        'margarida vieira',
-        'chen yajun',
-        'jason bryant',
-        'myocor',
-        'back to life'                  ,  # 'collaborator defined for study NCT03616639  Ignore'
+        'asklepios klinik altona'       ,  # 'q732015 architectural structure
+        'back to life'                  ,  # 'collaborator defined for study nct03616639  ignore'
+        'balamurali ambati',
+        'benjamin movsas, m.d.'         ,  # 'q4889047 american physician
+        'bill mcghee'                   ,  # 'q4910107 american baseball player
+        'brenda cooper, md'             ,  # 'q4960658 american writer
+        'campbell grant'                ,  # 'q2935481 us-american animator and dubbi
         'careggi hospital',
+        'carol johnston',
+        'caroline nilsson'              ,  # 'q18685243 american politician
+        'chen xiangmei'                 ,  # 'q545822 american politician
+        'chen yajun',
         'chen-yu chen',
-        'christian nickel'              ,  # 'Q26272221 actor
+        'chikara tashiro'               ,  # 'q11576125 association football player
+        'chloe scott'                   ,  # 'q47067499 american pornographic actress
+        'chloe scott'                   ,  # 'q47067499 american pornographic actress
+        'christian kern',
+        'christian nickel'              ,  # 'q26272221 actor
+        'david minor, md'               ,  # 'q43199624 movie theater in eugene, orego
+        'ditte hansen'                  ,  # 'q12308738 danish actress
+        'donnenfeld, eric, m.d.'        ,  # 'q694508 association footballer
+        'envida, maastricht, the netherlands'       ,  # 'q24250852 (1806-1859) soldier and public
+        'euraxi',
+        'fengxi su',
+        'florian beigel'                ,  # 'q19661459 architect based in london
         'gammacan',
+        'george brewer'                 ,  # 'q5537230 british writer
+        'grant jones'                   ,  # 'q5596289 american landscape architect
+        'holger thiele'                 ,  # 'q562164 american astronomer
+        'hongwen xu',
         'irenbe',
-        'tony eissa'                    ,  # 'Q22688616 actor
-        'sunil rao'                     ,  # 'Q7640340 actor
-        'stephens & associates, inc.',
-        'tracie collins, MD, MPH'       ,  # 'Q23932108 actor
-        'joseph hazelton'               ,  # 'Q6283869 American actor
-        'michael rosenbaum'             ,  # 'Q311613 American actor
-        'michelle lopez',
-        'jeffrey kramer, MD'            ,  # 'Q6176107 American actor and producer
+        'james baumgartner, md'         ,  # 'q6133274 american logician
+        'jason bryant',
+        'jeffrey browning'              ,  # 'q16208757 american ultramarathon runner
+        'jeffrey kramer, md'            ,  # 'q6176107 american actor and producer
+        'jennifer gay'                  ,  # 'q6178342 british television announcer
+        'jerry zimmerman'               ,  # 'q6184702 american baseball players
+        'joel lavine',
+        'jonathan berman',
+        'joseph dib',
+        'joseph hazelton'               ,  # 'q6283869 american actor
         'julian pine',
         'julie wang',
-        'marc breton'                   ,  # 'Q3287847 French actor
-        'andreas guenther'              ,  # 'Q497666 German actor
-        'abdulkadir tunc'               ,  # 'Q15043411 Turkish actor
-        'campbell grant'                ,  # 'Q2935481 US-American animator and dubbi
-        'linda carlson'                 ,  # 'Q11832719 actress
-        'lisa brenner'                  ,  # 'Q461378 American actress
-        'chloe scott'                   ,  # 'Q47067499 American pornographic actress
-        'fengxi su',
-        'kaitlyn kelly, MD'             ,  # 'Q437226 American pornographic actress
-        'kirsten williams'              ,  # 'Q6416126 Canadian actress
-        'ditte hansen'                  ,  # 'Q12308738 Danish actress
-        'mindframe, inc.'               ,  # 'Q42607297 movie theater in Dubuque, Iowa
-        'david minor, MD'               ,  # 'Q43199624 movie theater in Eugene, Orego
-        'envida, maastricht, the netherlands'       ,  # 'Q24250852 (1806-1859) soldier and public
-        'sally falk'                    ,  # 'Q7405128 American anthropologist
-        'holger thiele'                 ,  # 'Q562164 American astronomer
-        'oscar goodman, jr.'            ,  # 'Q515259 American attorney and politici
-        'bill mcghee'                   ,  # 'Q4910107 American baseball player
-        'jerry zimmerman'               ,  # 'Q6184702 American baseball players
-        'justin watts'                  ,  # 'Q16233655 American basketball player
-        'lindsey miller'                ,  # 'Q6552896 American judge
-        'grant jones'                   ,  # 'Q5596289 American landscape architect
-        'james baumgartner, MD'         ,  # 'Q6133274 American logician
-        'benjamin movsas, M.D.'         ,  # 'Q4889047 American physician
-        'ronald hoffman'                ,  # 'Q7364939 American physician
-        'paul carlson'                  ,  # 'Q15491063 American physician
-        'kathryn stephenson'            ,  # 'Q6377129 American physician
-        'stephen trokel'                ,  # 'Q1599537 American physicist
-        'chen xiangmei'                 ,  # 'Q545822 American politician
-        'andrew brenner'                ,  # 'Q4756413 American politician
-        'caroline nilsson'              ,  # 'Q18685243 American politician
-        'chloe scott'                   ,  # 'Q47067499 American pornographic actress
-        'kaitlyn kelly, MD'             ,  # 'Q437226 American pornographic actress
-        'natalie allen'                 ,  # 'Q17438529 American presenter, anchor and
-        'mary fristad'                  ,  # 'Q45882355 American psychologist and acad
-        'stefan g. hofmann'             ,  # 'Q37837264 American Psychology professor
-        'kelly tanner'                  ,  # 'Q16194830 American stock car racing driv
-        'adam friedman'                 ,  # 'Q2823944 American television producer a
+        'justin watts'                  ,  # 'q16233655 american basketball player
+        'k. lieb',
+        'kaitlyn kelly, md'             ,  # 'q437226 american pornographic actress
+        'kaitlyn kelly, md'             ,  # 'q437226 american pornographic actress
+        'kathryn stephenson'            ,  # 'q6377129 american physician
+        'kelly tanner'                  ,  # 'q16194830 american stock car racing driv
+        'kirsten williams'              ,  # 'q6416126 canadian actress
+        'koyce fox'                     ,  # 'q6297486 american writer
+        'lfce',
+        'linda carlson'                 ,  # 'q11832719 actress
+        'lindsey miller'                ,  # 'q6552896 american judge
+        'lisa brenner'                  ,  # 'q461378 american actress
+        'marc breton'                   ,  # 'q3287847 french actor
+        'margarida vieira',
+        'maria sklodowska-curie institute - oncology center'       ,  # 'q1054401 architectural structure
+        'mary fristad'                  ,  # 'q45882355 american psychologist and acad
+        'michael j. rosen, md.'         ,  # 'q6831494 american writer
+        'michael rosenbaum'             ,  # 'q311613 american actor
+        'michelle lopez',
+        'mindframe, inc.'               ,  # 'q42607297 movie theater in dubuque, iowa
+        'myocor',
+        'nailab, kenya'                 ,  # 'q6959471 businessperson
+        'nancy bauman',
+        'natalie allen'                 ,  # 'q17438529 american presenter, anchor and
+        'natasha duke'                  ,  # 'q28802903 animator
         'obstetrix',
-        'sharon walsh'                  ,  # 'Q461080 American tennis player
-        'tularik',
-        'joseph dib',
-        'syral',
-        'samantha harrison',
-        'hongwen xu',
-        'jeffrey browning'              ,  # 'Q16208757 American ultramarathon runner
-        'jonathan berman',
-        'euraxi',
-        'tobias werther',
-        'koyce fox'                     ,  # 'Q6297486 American writer
-        'brenda cooper, md'             ,  # 'Q4960658 American writer
-        'michael j. rosen, md.'         ,  # 'Q6831494 American writer
-        'natasha duke'                  ,  # 'Q28802903 animator
-        'airfan'                        ,  # 'Q55760547 association football player
-        'chikara tashiro'               ,  # 'Q11576125 association football player
-        'donnenfeld, eric, m.d.'        ,  # 'Q694508 Association footballer
-        'florian beigel'                ,  # 'Q19661459 architect based in London
-        'maria sklodowska-curie institute - oncology center'       ,  # 'Q1054401 architectural structure
-        'asklepios klinik altona'       ,  # 'Q732015 architectural structure
-        'Mary Knudson, M.D.'            ,  # 'Q58051223 anthropologist
-        'SA Ambulance Service'          ,  # 'Q7388884 Australian ambulance service
-        'Byron Lam'                     ,  # 'Q5004408 Australian botanist
-        'Brian Walters'                 ,  # 'Q4965569 Australian politician
-        'Brendan Lee'                   ,  # 'Q4960963 Australian rules footballer
-        'David Birnie'                  ,  # 'Q377241 Australian serial killer
-        'Lucy Holmes, MD'               ,  # 'Q6698364 Australian singer
-        'Jacob Rosenberg'               ,  # 'Q6119196 Australian writer
-        'Oliver Strohm'                 ,  # 'Q686257 Austrian ski jumper
-        'Shashi Bhatt, MD'              ,  # 'Q13487172 badminton player
-        'Johannes Wacker, MD'           ,  # 'Q27906334 badminton player
-        'BaroNova, Inc.'                ,  # 'Q2467415 ballerina
-        'Colaris, Joost, M.D.'          ,  # 'Q1107605 band
-        'David A. Johnson, MD'          ,  # 'Q1038597 baseball player
-        'Cengiz KAYA'                   ,  # 'Q6037195 basketball player
-        'Brigade de Sapeurs Pompiers de Paris'       ,  # 'Q583422 brigade
-        'Kenneth Hargreaves'            ,  # 'Q6390187 British Army officer
-        'Michael Levy'                  ,  # 'Q337010 British Baron
-        'Julie Page'                    ,  # 'Q1639835 British basketball player
-        'Christopher Sweeney, MBBS'     ,  # 'Q17306850 British Director
-        'Caroline Rowland'              ,  # 'Q5045208 British film producer
-        'Krystal Parker'                ,  # 'Q6439758 British footballer
-        'Nicholas Kenyon'               ,  # 'Q7025709 British journalist
-        'Michael Gallagher'             ,  # 'Q6830494 British miner, politician and
-        'Kevin Davy'                    ,  # 'Q6396118 British musician
-        'Henry C. Lin, MD'              ,  # 'Q21453388 British painter (1877-1965)
-        'Jenny Tong, MD, MPH'           ,  # 'Q334348 British politician
-        'Joseph Bliss'                  ,  # 'Q6281570 British politician
-        'ARGOS'                         ,  # 'Q4789707 British retail company
-        'Alison Walker'                 ,  # 'Q4727205 British sports broadcaster
-        'Jennifer Gay'                  ,  # 'Q6178342 British television announcer
-        'George Brewer'                 ,  # 'Q5537230 British writer
-        'NaiLab, Kenya'                 ,  # 'Q6959471 Businessperson
+        'oscar goodman, jr.'            ,  # 'q515259 american attorney and politici
         'ovo r & d',
+        'paul carlson'                  ,  # 'q15491063 american physician
+        'rachel little',
+        'ronald hoffman'                ,  # 'q7364939 american physician
+        'sally falk'                    ,  # 'q7405128 american anthropologist
+        'salvador gil-vernet',
+        'samantha harrison',
+        'sharon walsh'                  ,  # 'q461080 american tennis player
+        'stefan g. hofmann'             ,  # 'q37837264 american psychology professor
+        'stephen trokel'                ,  # 'q1599537 american physicist
+        'stephens & associates, inc.',
+        'sunil rao'                     ,  # 'q7640340 actor
+        'syral',
+        'tobias werther',
+        'tony eissa'                    ,  # 'q22688616 actor
+        'tracie collins, md, mph'       ,  # 'q23932108 actor
+        'tularik',
         'vesalio',
+        'victoria gomez',
+        'vincent kan',
+        'yijing he',
       ]
     end
   end
