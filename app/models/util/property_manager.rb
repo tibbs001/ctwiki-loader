0990e87c74bf70561585a0c3ea96673f059b9128
tibbs001 <sheri.tibbs@duke.edu>
2019-02-08 16:09:03 -0500

    If property not found, fail gracefully

--- app/models/util/property_manager.rb
+++ app/models/util/property_manager.rb
@@ -12,6 +12,10 @@ module Util
       # For now, assume there can only be 1 or 2 methods to get to the values
       # For consistency, return an array even if there's only one value
       methods = property_map[code]
+      if methods.nil?
+        puts "WARNING:  Property #{code} not found."
+        return []
+      end
       return [study.send(methods.first)] if methods.size == 1
       return study.send(methods.first).pluck(methods.last).uniq
     end
