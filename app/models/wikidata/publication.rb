873f33ceae0881ea5e52421f964c98c5ceb88f47
tibbs001 <sheri.tibbs@duke.edu>
2019-05-17 13:04:00 -0400

    Let Publication populate itself.

--- app/models/wikidata/publication.rb
+++ app/models/wikidata/publication.rb
@@ -10,6 +10,36 @@ module Wikidata
       self.pmid = hash[:pmid]
     end
 
+    def populate
+      #pmids=Ctgov::StudyReference.where("reference_type='results_reference'").pluck(:pmid).compact
+      sample_pmids.each {|pmid|
+        not_loaded = (where('pmid=?',pmid).first) == nil
+        not_in_wikidata = (Lookup::Publication.where('pmid=?',pmid).first) == nil
+        if not_loaded and not_in_wikidata
+          xml=client.get_xml_for(pmid)
+          Wikidata::Publication.new({xml: xml, pmid: pmid}).create
+        end
+      }
+    end
+
+    def sample_pmids
+       ['20025029',
+       '19648180',
+       '17923590',
+       '30325889',
+       '16188811',
+       '26473001',
+       '26874298',
+       '16989935',
+       '18492509',
+       '11900034',
+       '30538976',
+       '29895572',
+       '15036740',
+       '20483522',
+       '30262463']
+    end
+
     def create
       ActiveRecord::Base.logger=nil
       p=Wikidata::Publication.where('pmid=?',pmid).first
