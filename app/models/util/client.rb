679e27c0250b17d89daabf0708a8d1f68e2a9646
tibbs001 <sheri.tibbs@duke.edu>
2019-05-26 15:59:35 -0400

    ct-017:  For some reason, sending a request to the ncbi entrez API with Faraday results in an ‘abuse error’, but using curl to send the request works.  It seems clunky, but using curl to retrieve xml and save to a file.

--- app/models/util/client.rb
+++ app/models/util/client.rb
@@ -6,14 +6,21 @@ module Util
       tries ||= 5
       begin
         #url="#{BASE_URL}&id=#{pmid}&retmode=xml&rettype=abstract"
-        #https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=16002928
+        #https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=29895572
         url="#{BASE_URL}&id=#{pmid}"
         puts url
-        xml=Nokogiri::XML(Faraday.get(url).body)
+        # For some reason, this command now makes the entrez API think we're committing abuse and returns:
+        #https://misuse.ncbi.nlm.nih.gov/error/abuse.shtml?db=pubmed&amp;id=29895572&amp;retmode=xml
+        # So just pull the xml down and save locally for now.  Have emailed ncbi support
+        #xml=Nokogiri::XML(Faraday.get(url).body)
+        xml_file_name="spec/support/xml_data/#{pmid}.xml"
+        system("curl -k '#{url}' > #{xml_file_name}")
+        file = File.open(xml_file_name)
         sleep(2.seconds)
-        return xml
+        return Nokogiri::XML(file)
       rescue => e
         #  have been encountering timeout errors.  If encountered, try again
+        sleep(2.seconds)
         if (tries -=1) > 0
           puts "Error calling: #{url}"
           puts e.inspect
