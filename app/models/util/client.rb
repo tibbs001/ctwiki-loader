568a2a6dc605776244f8a3ba9682e592f3a42a15
tibbs001 <sheri.tibbs@duke.edu>
2019-07-30 09:40:17 -0700

    Add method that lets us load data from files on the local machine.

--- app/models/util/client.rb
+++ app/models/util/client.rb
@@ -2,6 +2,23 @@ module Util
   class Client
     BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml'
 
+    def get_local_xml_for(pmid)
+      tries ||= 5
+      begin
+        xml_file_name="/aact-files/xml_downloads/pubmed_xml/#{pmid[0]}/#{pmid}.xml"
+        file = File.open(xml_file_name)
+        return Nokogiri::XML(file)
+      rescue => e
+        #  have been encountering timeout errors.  If encountered, try again
+        if (tries -=1) > 0
+          puts "Error getting: #{xml_file_name}"
+          puts e.inspect
+          retry
+        end
+        puts "Giving up on #{pmid}. Move on to next publication"
+      end
+    end
+
     def get_xml_for(pmid)
       tries ||= 5
       begin
@@ -13,7 +30,6 @@ module Util
         #https://misuse.ncbi.nlm.nih.gov/error/abuse.shtml?db=pubmed&amp;id=29895572&amp;retmode=xml
         # So just pull the xml down and save locally for now.  Have emailed ncbi support
         #xml=Nokogiri::XML(Faraday.get(url).body)
-        xml_file_name="/aact-files/xml_downloads/pubmed_xml/#{pmid[0]}/#{pmid}.xml"
         system("curl -k '#{url}' > #{xml_file_name}")
         file = File.open(xml_file_name)
         sleep(2.seconds)
--- app/models/util/updater.rb
+++ app/models/util/updater.rb
@@ -2,6 +2,14 @@ module Util
 
   class Updater
 
+    def load_local_pubs
+      lookup_mgr = Util::LookupManager.new
+      Ctgov::StudyReference.all.pluck(:pmid).uniq.each {|pmid|
+        xml=Util::Client.new.get_local_xml_for(pmid)
+        Pubmed::Publication.new({xml: xml, pmid: pmid, :lookup_mgr=>lookup_mgr}).create
+      }
+    end
+
     def load_pubs
       lookup_mgr = Util::LookupManager.new
       not_yet_in_wikidata = Lookup::Publication.where('qcode is null').pluck(:pmid)
