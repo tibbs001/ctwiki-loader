770b80808cc5ebe2339e1ac47767db045fd49a8c
tibbs001 <sheri.tibbs@duke.edu>
2019-07-31 13:38:05 -0400

    Add method to load pubmed from files

--- app/models/util/updater.rb
+++ app/models/util/updater.rb
@@ -10,6 +10,23 @@ module Util
       }
     end
 
+    def load_pub_files
+      lookup_mgr = Util::LookupManager.new
+      cntr = 1
+      while cntr < 10 {
+        dir_name="/aact-files/xml_downloads/pubmed_xml/#{cntr}/"
+        file_names=Dir.entries(dir_name) - ['.','..']
+        file_names.each { |file_name|
+          pmid = file_name.split('.').first
+          xml_file_name="/aact-files/xml_downloads/pubmed_xml/#{cntr}/#{pmid}.xml"
+          file = File.open(xml_file_name)
+          xml= Nokogiri::XML(file)
+          Pubmed::Publication.new({xml: xml, pmid: pmid, :lookup_mgr=>lookup_mgr}).create
+        }
+        cntr = cntr + 1
+      }
+    end
+
     def load_pubs
       lookup_mgr = Util::LookupManager.new
       not_yet_in_wikidata = Lookup::Publication.where('qcode is null').pluck(:pmid)
