383501e57431a589787d0968aaefaf4a02c29ab6
Sheri Tibbs <sheri.tibbs@gmail.com>
2020-04-21 17:11:09 +0000

    prepare to start adding label/description in other languages

--- app/models/ctgov/study.rb
+++ app/models/ctgov/study.rb
@@ -61,8 +61,12 @@ module Ctgov
     end
 
     def prop_codes
-      ['Len',    # label
-       'Den',    # description
+      ['Len',    # English label
+       'Den',    # English description
+       'Lnl',    # Dutch label
+       'Dnl',    # Dutch description
+       'Les',    # Spanish label
+       'Des',    # Spanish description
        'P17',    # countries
        'P31',    # instance of
        'P248',   # publications
@@ -90,7 +94,11 @@ module Ctgov
         when 'Len'
           return compose_label(reg_prefix)
         when 'Den'
-          return "#{reg_prefix}\"clinical trial\""     # Description
+          return "#{reg_prefix}\"clinical trial\""     # English Description
+        when 'Dnl'
+          return "#{reg_prefix}\"klinisch onderzoek\"" # Dutch Description
+        when 'Des'
+          return "#{reg_prefix}\"ensayo clínico\""     # Spanish Description
         when 'P17'    # country
           return country_quickstatements
         when 'P31'    # instance of
