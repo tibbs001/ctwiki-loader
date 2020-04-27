69a1259a74b26fad06128efe6275269d38f5a095
Sheri Tibbs <sheri.tibbs@gmail.com>
2020-04-27 22:33:07 +0000

    get the assign_existing_studies_missing_prop method working again

--- app/models/util/study_prepper.rb
+++ app/models/util/study_prepper.rb
@@ -14,22 +14,25 @@ module Util
       QsCreator::Study.new
     end
 
+    def get_id_maps
+      lookup_mgr.studies
+    end
+
     def assign_existing_studies_missing_prop(code)
       # method to create a file of single snaks for just one property
+      qsc=QsCreator::Study.new
+      qsc.set_delimiters
       File.open("public/assign_#{code}.txt", "w+") do |f|
         mgr.ids_for_studies_without_prop(code).each {|hash|
           nct_id = hash.keys.first
-          study = source_model_name.get_for(nct_id, lookup_mgr)
+          study = qsc.get_for(nct_id)
           if study
-            study.subject = hash.values.first
-            f << study.quickstatement_for(code)
+            qsc.subject = hash.values.first
+            f << qsc.quickstatement_for(code)
           end
         }
       end
     end
 
-    def get_id_maps
-      lookup_mgr.studies
-    end
   end
 end
